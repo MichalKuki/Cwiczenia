20e7c17022bde8395f711d718e7f10eb333a87e5
MichalKuki <114807829+MichalKuki@users.noreply.github.com>
2023-02-19 09:57:27 +0100

    Update CelsiusToFahrenheit.java

--- CelsiusToFahrenheit.java
+++ CelsiusToFahrenheit.java
@@ -1,13 +1,16 @@
 import javax.swing.*;
-
-public class CelsiusToFahrenheit extends JFrame
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+public class CelsiusToFahrenheit extends JFrame implements ActionListener
 {
+    JLabel lCelcius, lFarenheit;
+    JTextField tCelsius, tFarenheit;
+    JButton bKonweruj;
+    JCheckBox chWielkie;
+    double tempCelcius, tempFarenheit;
     public CelsiusToFahrenheit()
     {
-        JLabel lCelcius, lFarenheit;
-        JTextField tCelsius, tFarenheit;
-        JButton bKonweruj;
-
 
         setSize(400,200);
         setTitle("Przeliczanie stopni celcjusza na Farenhaita");
@@ -20,20 +23,48 @@ public class CelsiusToFahrenheit extends JFrame
         tCelsius = new JTextField("");
         tCelsius.setBounds(170,20,150,20);
         add(tCelsius);
+        tCelsius.addActionListener(this);
 
-        lFarenheit = new JLabel("Stopnie Farengeita: ");
-        lFarenheit.setBounds(70,20,150,20);
-        add(lFarenheit);
+      lFarenheit = new JLabel("Stopnie Farenheita ");
+      lFarenheit.setBounds(20, 70,150,20);
+      add(lFarenheit);
 
-        tFarenheit = new JTextField("");
-        tFarenheit.setBounds(170,20,150,20);
-        add(tFarenheit);
+      tFarenheit = new JTextField(" ");
+      tFarenheit.setBounds(170, 70, 150, 20);
+      add(tFarenheit);
+
+      bKonweruj = new JButton("Konwertuj ");
+      bKonweruj.setBounds(70, 100 ,150,20);
+      add(bKonweruj);
+      bKonweruj.addActionListener(this);
+
+      chWielkie = new JCheckBox("Wielkie litery ");
+      chWielkie.setBounds(250,100,150,20);
+      add(chWielkie);
+      chWielkie.addActionListener(this);
 
 
-        bKonweruj = new JButton("Konwertuj");
-        bKonweruj.setBounds(20, 20,150,20);
-        add(bKonweruj);
     }
+     @Override
+     public void actionPerformed(ActionEvent e) {
+        Object zrodlo = e.getSource();
+        if (zrodlo == bKonweruj || zrodlo == tCelsius) {
+
+            tempCelcius = Double.parseDouble(tCelsius.getText());
+            tempFarenheit = 32.0 + (9.0 / 5) * tempCelcius;
+            tFarenheit.setText(String.valueOf(tempFarenheit));
+        }
+        else if (zrodlo == chWielkie)
+        {
+            if (chWielkie.isSelected()==true)
+            {
+                tFarenheit.setFont(new Font("SansSerif", Font.BOLD, 18));
+            }
+            else {
+                tFarenheit.setFont(new Font("SansSerif", Font.BOLD, 12));
+            }
+        }
+     }
 
     public static void main(String[] args)
     {
