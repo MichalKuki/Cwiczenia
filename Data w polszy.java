b09db9f73f62957e185df51b2df65a3c1a050d0a
MichalKuki <114807829+MichalKuki@users.noreply.github.com>
2023-02-25 10:37:18 +0100

    Add files via upload

---
+++ Data w polszy.java
@@ -0,0 +1,11 @@
+import javax.xml.crypto.Data;
+import java.util.Date;
+
+public class Main {
+    public static void main(String[] args) {
+        Date date = new Date ();
+        System.out.println("W Polsce jest: ");
+        System.out.println(date);
+    }
+}
+
