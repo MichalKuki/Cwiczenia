a9437163de717fff07300518a89125f2c9ee0b85
MichalKuki <114807829+MichalKuki@users.noreply.github.com>
2023-02-25 11:58:27 +0100

    Add files via upload

---
+++ tablica.java
@@ -0,0 +1,24 @@
+public class Main {
+    public static void main(String[] args) {
+        int[]  tablica1 = {6,2,7,9,3,7,9,22};
+        double sumaTab = 0;
+        double roznicaTab = 0;
+        double iloczynTab = 1;
+        double ilorazTab = 1;
+
+        //"Wypisuje elementy tablicy a i suma
+        for (int i = 0; i < tablica1.length; i++) {
+            System.out.println("Element w tablicy a o indeksie " + i + " czyli pierwszy, to: "  + tablica1[i]);
+            sumaTab += tablica1[i];
+            roznicaTab -= tablica1[i];
+            iloczynTab *= tablica1[i];
+            ilorazTab /= tablica1[i];
+        }
+        System.out.println("--------------------------------------------------------------");
+        System.out.println("Suma elementów tablicy a wynosi " + sumaTab);
+        System.out.println("Różnica elementów tablicy a wynosi " + roznicaTab);
+        System.out.println("Iloczyn elementów tablicy a wynosi " + iloczynTab);
+        System.out.println("Iloraz elementów tablicy a wynosi " + ilorazTab);
+
+    }
+}
